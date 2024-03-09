e59da198ed7b7113d01d1f2b977879dab68951cf
Gus Esquivel <gesquive@gmail.com>
2024-03-08 23:07:33 -0600

    add some dependency checks

--- get_latest_date.sh
+++ get_latest_date.sh
@@ -1,3 +1,18 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
+
+# Check required dependencies
+readonly DEPENDENCIES="find xargs sort cut head"
+for dependency in ${DEPENDENCIES}; do
+  if ! command -v "${dependency}" > /dev/null 2>&1; then
+    echo "ERROR: command '${dependency}' not found" >&2
+    exit 2
+  fi
+done
+
+if [ $# -ne 1 ]; then
+  echo "Error: not enough arguments!"
+  echo "Usage is: $0 path"
+  exit 2
+fi
 
 find "$1" -type f -print0 | xargs -0 stat --format '%Y :%y %n' | sort -nr | cut -d: -f2- | head -n10
